e617f2df5ae21726ee53e2fe0adba08c90882916
ToiLaDuyGitHub <toiladuygg@gmail.com>
2025-05-16 16:32:58 +0100

    Tao code hello world

--- src/java/controller/EmptyController.java
+++ src/java/controller/EmptyController.java
@@ -9,5 +9,7 @@ package controller;
  * @author ToiLaDuyGitHub
  */
 public class EmptyController {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
